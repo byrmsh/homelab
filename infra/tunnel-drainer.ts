8515e6c027dabb2c1fdec062067e7f9cb53aa014
Bayram Şahin <me@bayram.sh>
2026-01-16 02:32:17 +0300

    feat(infra): enhance tunnel drainer with improved status checks

--- infra/tunnel-drainer.ts
+++ infra/tunnel-drainer.ts
@@ -8,44 +8,53 @@ interface CfTunnelDrainerInputs {
   apiToken: CfString
 }
 
+// The status of the tunnel. Valid values are:
+// - inactive (tunnel has never been run),
+// - degraded (tunnel is active and able to serve traffic but in an unhealthy state),
+// - healthy (tunnel is active and able to serve traffic), or
+// - down (tunnel can not serve traffic as it has no connections to the Cloudflare Edge).
+// SOURCE: https://developers.cloudflare.com/api/resources/zero_trust/subresources/tunnels/subresources/cloudflared/methods/list/
+const PossibleCfTunnelStatuses = ['inactive', 'degraded', 'healthy', 'down'] as const
+type CfTunnelStatus = (typeof PossibleCfTunnelStatuses)[number]
+
+const checkStatus = async (accountId: string, tunnelId: string, apiToken: string) => {
+  const url = `https://api.cloudflare.com/client/v4/accounts/${accountId}/cfd_tunnel/${tunnelId}`
+  const headers = { Authorization: `Bearer ${apiToken}`, 'Content-Type': 'application/json' }
+  const response = await fetch(url, { method: 'GET', headers })
+  if (!response.ok) throw new Error(`API Error ${response.status}: ${response.statusText}`)
+  const data = await response.json()
+  const status = data.result?.status
+  if (!status) throw new Error('Unexpected API response structure')
+  if (PossibleCfTunnelStatuses.includes(status)) return status as CfTunnelStatus
+  throw new Error('Unexpected API response value for tunnel status')
+}
+
+const checkStatusUntilDrained = async (accountId: string, tunnelId: string, apiToken: string) => {
+  const status = await checkStatus(accountId, tunnelId, apiToken)
+  if (status === 'inactive' || status === 'down') {
+    console.log(`Tunnel status is '${status}'. Proceeding with destruction.`)
+    return
+  }
+  console.log(`Tunnel status is '${status}'. Waiting 2s...`)
+  await new Promise(r => setTimeout(r, 2000))
+  return checkStatusUntilDrained(accountId, tunnelId, apiToken)
+}
+
 const cfTunnelDrainerProvider: pulumi.dynamic.ResourceProvider = {
-  async create(inputs: CfTunnelDrainerInputs) {
-    return { id: 'drainer-' + inputs.tunnelId, outs: inputs }
+  create(inputs: CfTunnelDrainerInputs) {
+    return Promise.resolve({ id: 'drainer-' + inputs.tunnelId, outs: inputs })
   },
 
-  async delete(_id: string, props: CfTunnelDrainerInputs) {
-    const { accountId, tunnelId, apiToken } = props
-    console.log(`Starting drain for tunnel: ${tunnelId}`)
-
-    while (true) {
-      const url = `https://api.cloudflare.com/client/v4/accounts/${accountId}/cfd_tunnel/${tunnelId}`
-
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${apiToken}`,
-          'Content-Type': 'application/json',
-        },
+  delete: (_id: string, props: CfTunnelDrainerInputs) =>
+    new Promise<void>((res, rej) => {
+      const { accountId, tunnelId, apiToken } = props
+      pulumi.all([accountId, tunnelId, apiToken]).apply(([accountId, tunnelId, apiToken]) => {
+        console.log('Starting drain for tunnel')
+        checkStatusUntilDrained(accountId, tunnelId, apiToken)
+          .then(() => res())
+          .catch(err => rej(err))
       })
-
-      if (!response.ok) {
-        if (response.status === 404) break
-        console.warn(`API Error ${response.status}: ${response.statusText}`)
-      } else {
-        const data = await response.json()
-        const status = data.result?.status
-
-        if (status === 'inactive') {
-          console.log(`Tunnel status is '${status}'. Proceeding with destruction.`)
-          break
-        }
-
-        console.log(`Tunnel status is '${status}'. Waiting 2s...`)
-      }
-
-      await new Promise(r => setTimeout(r, 2_000))
-    }
-  },
+    }),
 }
 
 export class CfTunnelDrainer extends pulumi.dynamic.Resource {
