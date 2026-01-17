e569e212aa5b4b628b1baf319b1eeed89e9379e7
Bayram Şahin <me@bayram.sh>
2026-01-17 19:30:54 +0300

    refactor(infra): extract cloud-init to template

--- infra/utils.ts
+++ infra/utils.ts
@@ -1,11 +1,10 @@
-import * as pulumi from '@pulumi/pulumi'
-
-type PulumiValue<T> = T | pulumi.Output<T> | pulumi.Input<T>
-
 export const enumerate = (count: number) => Array.from({ length: count }, (_, i) => i)
 
 export const generateServerName = (group: string, i: number) =>
   `${group}-` + i.toString().padStart(2, '0')
 
-export const indentOutputText = (text: PulumiValue<string>, spaceCount: number) =>
-  pulumi.output(text).apply(t => t.split('\n').join(`\n${' '.repeat(spaceCount)}`))
+export const fillTemplate = (template: string, replacements: Record<string, string>) =>
+  Object.entries(replacements).reduce(
+    (draft, [key, value]) => draft.replace(`[[${key}]]`, value),
+    template,
+  )
